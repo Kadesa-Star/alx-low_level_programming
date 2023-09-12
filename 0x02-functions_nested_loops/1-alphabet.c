957fb512a25fed063434bfe0b87d2df769710c93
Kadesa-Star <133206743+Kadesa-Star@users.noreply.github.com>
2023-09-12 14:12:59 -0700

    the alphabet

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,13 @@
+#include "main.h"
+/**
+ * printing_alphabet - in lower case
+ *
+ * Return: void
+ */
+void print_alphabet(void)
+{
+	char ch;
+	for (ch = 'a'; ch <= 'z'; ch++)
+		_putchar(ch);
+	_putchar('\n');
+}
