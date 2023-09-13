002fb30233fc70f1f1248dd3bf740aa98e3dc9a9
Kadesa-Star <133206743+Kadesa-Star@users.noreply.github.com>
2023-09-12 17:31:28 -0700

    adding two digits

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,11 @@
+#include "main.h"
+/**
+ * add - adds two integers
+ * @m1: the first number
+ * @m2: the second digit
+ * Return: integer value
+ */
+int add(int m1, int m2)
+{
+	return (m1 + m2);
+}
