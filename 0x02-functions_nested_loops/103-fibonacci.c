db66b59dd993047926af3c1c8695d5a60bb20508
Kadesa-Star <133206743+Kadesa-Star@users.noreply.github.com>
2023-09-13 05:34:09 -0700

    even liber abbaci

---
+++ 0x02-functions_nested_loops/103-fibonacci.c
@@ -0,0 +1,27 @@
+#include <stdio.h>
+/**
+ * main - prints the sum of even valued terms
+ * Return: 0 (success)
+ */
+int main(void)
+{
+	int i;
+	unsigned long int fb1 = 0;
+	unsigned long int fb2 = 1;
+	unsigned long int fbsum;
+
+	while (1)
+	{
+		fbsum = fb1 + fb2
+			if (fbsum > 4000000)
+				break;
+		if ((fbsum % 2) == 0)
+			total_sum += fbsum;
+
+		fb1 = fb2;
+		fb2 = fbsum;
+
+	}
+	printf("%.0f\n", total_sum);
+	return (0);
+}
