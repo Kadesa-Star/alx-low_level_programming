8a9ec3d9820d9c7b3e06d41201120b9bb5f61666
Kadesa-Star <133206743+Kadesa-Star@users.noreply.github.com>
2023-09-14 15:31:03 -0700

    distinguish prime and composite

---
+++ 0x04-more_functions_nested_loops/100-prime_factor.c
@@ -0,0 +1,22 @@
+#inclued <stdio.h>
+/**
+ * main - finds and prints the largest prime factor
+ *
+ * Return: 0 always
+ */
+int main(void)
+{
+	long prime = 612852475143;
+	long x, maxf;
+	double square = sqrt(prime);
+
+	for (x = 1; x <= square; x++)
+	{
+		if (prime % x == 0)
+		{
+			maxf = prime / x;
+		}
+	}
+	printf("%ld\n", maxf);
+	return (0);
+}
