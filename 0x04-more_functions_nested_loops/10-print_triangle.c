63e4ec92bb516847acc648336de85f1ea7a03a36
Kadesa-Star <133206743+Kadesa-Star@users.noreply.github.com>
2023-09-14 16:15:37 -0700

    triangles

--- 0x04-more_functions_nested_loops/10-print_triangle.c
+++ 0x04-more_functions_nested_loops/10-print_triangle.c
@@ -5,23 +5,26 @@
  */
 void print_triangle(int size)
 {
-	int hashtag, indexing;
-
-	if (size > 0)
+	if (size <= 0)
 	{
-		for (hashtag = 1; hashtag <= size; hashtag++)
+		_putchar('\n');
+	}
+	else
+	{
+
+		int i, j;
+
+		for (i = 1; i <= size; i++)
 		{
-			for (indexing = size - hashtag; indexing > 0; indexing++)
+			for (j = i; j < size; j++)
+			{
 				_putchar(' ');
-
-			for (indexing = 0; indexing < hashtag; indexing++)
+			}
+			for (j = 1; j <= i; j++)
+			{
 				_putchar('#');
-
-			if (hashtag == size)
-				continue;
-
+			}
 			_putchar('\n');
 		}
 	}
-	_putchar('\n');
 }
