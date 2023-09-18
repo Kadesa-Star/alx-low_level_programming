29a27297491f68e9dc2547f128b0fa6ad249af71
Kadesa-Star <133206743+Kadesa-Star@users.noreply.github.com>
2023-09-18 11:25:40 -0700

    length of a string

---
+++ 0x05-pointers_arrays_strings/2-strlen.c
@@ -0,0 +1,15 @@
+#include "main.h"
+/**
+ * _strlen - length of a string
+ * @s: character
+ * Return: length of the string
+ */
+int _strlen(char *s)
+{
+	int count;
+
+	for (count = 0; string[count] != '\0'; count++)
+
+	return (count);
+
+}
