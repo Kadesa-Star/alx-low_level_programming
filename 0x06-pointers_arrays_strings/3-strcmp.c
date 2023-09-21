8bd76683f23fe5b9a7150ebb5f6e27df5eb6ec7f
Kadesa-Star <133206743+Kadesa-Star@users.noreply.github.com>
2023-09-21 10:25:03 -0700

    comparison

---
+++ 0x06-pointers_arrays_strings/3-strcmp.c
@@ -0,0 +1,21 @@
+#include "main.h"
+/**
+ * _strcmp - copies a string
+ * @s1: first string
+ * @s2: second string
+ * Return: comparison
+ */
+int _strcmp(char *s1, char *s2)
+{
+		int k = 0;
+
+		while (s1 != '\0' && s2 != '\0')
+		{
+			if (s1[k] != s2[k])
+			{
+				return (s1[k] - s2[k]);
+			}
+			k++;
+		}
+		return (0);
+}
