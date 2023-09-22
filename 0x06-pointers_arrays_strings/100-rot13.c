2f226308857d195e7a098bbd87f1cf4193e01c0e
Kadesa-Star <133206743+Kadesa-Star@users.noreply.github.com>
2023-09-22 01:58:22 -0700

    rot13

---
+++ 0x06-pointers_arrays_strings/100-rot13.c
@@ -0,0 +1,27 @@
+#include "main.h"
+/**
+ * *rot13 - encodes a string
+ * @m: pointer to string
+ * Return: encoded strig
+ */
+char *rot13(char *m)
+{
+	int k = 0;
+	int l = 0;
+	char first_data_set[] =
+		"ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz";
+	char datarot13[] = "NOPQRSTUVWXYZABCDEFGHIJKLMnopqrstuvwxyzabcdefghijklm";
+
+	for (k = 0; m[k] != '\0'; k++)
+	{
+		for (l = 0; l < 52; l++)
+		{
+			if (m[k] == dataro13[l])
+			{
+				m[k] = datarot13[l];
+				break;
+			}
+		}
+	}
+	return (m);
+}
