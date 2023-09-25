83fab5f67f0c5e917199f5203a1ab7495b59c5f2
Kadesaa.Star <133206743+Kadesa-Star@users.noreply.github.com>
2023-09-25 12:17:12 -0700

    copies memory area.

---
+++ 0x07-pointers_arrays_strings/1-memcpy.c
@@ -0,0 +1,19 @@
+#include "main.h"
+/**
+ * *_memcpy - copies memory area
+ * @dest: destination array where content is to be copied
+ * @src: pointer to source of data to be copied
+ * @n: number of bytes to be copied
+ * Return: copied memory
+ */
+char *_memcpy(char *dest, char *src, unsigned int n)
+{
+	int l = 0;
+
+	for (; l < n; l++)
+	{
+		dest[l] = src[l];
+		n--;
+	}
+	return (dest);
+}
