032bde45c4462d4c15dc302b11d2412a0f625f24
Kadesa-Star <133206743+Kadesa-Star@users.noreply.github.com>
2023-11-02 13:04:37 -0700

    Write a function that concatenates two strings.

---
+++ 0x0C-more_malloc_free/1-string_nconcat.c
@@ -0,0 +1,46 @@
+#include "main.h"
+#include <stdio.h>
+#include <stdio.h>
+/**
+ * string_nconcat - a  funtion that concatenates two strings
+ * @s1: string 1
+ * @s2: second string
+ * @n: integer value
+ * Description: this is a funtion thstring_nconcatat concatenates two strings.
+ * Return: NULL if function fails and the concatenated string otherwise
+ */
+char *string_nconcat(char *s1, char *s2, unsigned int n)
+{
+	unsigned int lens1 = 0;
+	unsigned int lens2 = 0;
+	char *concst;
+	unsigned int k;
+
+	if (s1 == NULL || s2 == NULL)
+		s1 = "";
+		s2 = "";
+	for (k = 0; s1[k] != '\0'; k++)
+		lens1++;
+	for (k = 0; s2[k] != '\0'; k++)
+		lens2++;
+	concst = malloc(sizeof(char) * (lens1 + n) + 1);
+	if (concst == NULL)
+		return (NULL);
+	if (n >= lens2)
+	{
+		for (k = 0; s1[k] != '\0'; k++)
+			concst[k] = s1[k];
+		for (k = 0; s2[k] != '\0'; k++)
+			concst[lens1 + k] = s2[k];
+		concst[lens1 + k] = '\0';
+	}
+	else
+	{
+		for (k = 0; s1[k] != '\0'; k++)
+			concst[k] = s1[k];
+		for (k = 0; k < n; k++)
+			concst[lens1 + k] = s2[k];
+		concst[lens1 + k] = '\0';
+	}
+	return (concst);
+}
