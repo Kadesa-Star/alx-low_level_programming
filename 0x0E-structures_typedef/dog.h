8ca937b2427b14df7851929b8fa3b73c720e1557
Kadesa-Star <133206743+Kadesa-Star@users.noreply.github.com>
2023-11-07 03:21:53 -0800

    define a new type struct

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -1,6 +1,18 @@
+#ifndef DOG_H
+#define DOG_H
+/**
+ * struct dog - the structure
+ * @name: name of dog
+ * @age: age of dog
+ * @owner: owner of dog
+ *
+ * Description: creating a  new struct called dog
+ */
 struct dog
 {
 	char *name;
 	float age;
 	char *owner;
 };
+
+#endif
