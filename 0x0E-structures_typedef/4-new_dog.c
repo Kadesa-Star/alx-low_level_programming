a5933a27dec26cc2f537cf1f9fad82d862ce2f9f
Kadesa-Star <133206743+Kadesa-Star@users.noreply.github.com>
2023-11-07 11:40:07 -0800

    a function that creates a new dog

--- 0x0E-structures_typedef/4-new_dog.c
+++ 0x0E-structures_typedef/4-new_dog.c
@@ -71,7 +71,7 @@ dog_t *new_dog(char *name, float age, char *owner)
 		free(dog->name);
 		return (NULL);
 	}
-	_stcpy(dog->name, name);
+	_strcpy(dog->name, name);
 	_strcpy(dog->owner, owner);
 	dog->age = age;
 
