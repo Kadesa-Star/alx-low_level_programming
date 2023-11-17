e1c109cc3b4338a199c1aba20ebf2109423fa2ce
Kadesa-Star <133206743+Kadesa-Star@users.noreply.github.com>
2023-11-17 07:27:36 -0800

    print elements of a linked list

--- 0x12-singly_linked_lists/0-print_list.c
+++ 0x12-singly_linked_lists/0-print_list.c
@@ -15,13 +15,13 @@ size_t print_list(const list_t *h)
 		if (h->str == NULL)
 			printf("[0] (nil)\n");
 		else
+		{
 			printf("[%d], %s\n", h->len, h->str);
+		}
 
+		cnt++;
+
+		h = h->next;
 	}
-
-	cnt++;
-
-	h = h->next;
-
 	return (cnt);
 }
