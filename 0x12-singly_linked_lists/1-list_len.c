8aedb54cfe74bba685ef99c22fcd6cf1504f137d
Kadesa-Star <133206743+Kadesa-Star@users.noreply.github.com>
2023-11-17 07:40:02 -0800

    number of elements

---
+++ 0x12-singly_linked_lists/1-list_len.c
@@ -0,0 +1,18 @@
+#include "lists.h"
+/**
+ * list_len - returns elements in a linked list
+ * @h: pointer to next address
+ *
+ * Return: the number of elements
+ */
+size_t list_len(const list_t *h)
+{
+	size_t num = 0;
+
+	if (h)
+	{
+		num = num + 1;
+		h = h->next;
+	}
+	return (num);
+}
