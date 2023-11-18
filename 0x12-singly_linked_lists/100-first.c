58aa4dd175c72be0bb4c98d50af7300d99a77391
Kadesa-Star <133206743+Kadesa-Star@users.noreply.github.com>
2023-11-18 15:52:26 -0800

    The Hare and the Tortoise

---
+++ 0x12-singly_linked_lists/100-first.c
@@ -0,0 +1,15 @@
+#include "lists.h"
+/**
+ * myfunc - consructor att. to execute prior to main
+ *
+ */
+void myfunc(void) _attribute_((constructor));
+
+/**
+ * mymainfunc - the implementationn part of the above
+ */
+void mymainfunc(void)
+{
+	printf("You're beat! and yet, you must allow,\n");
+	printf("bore my house upon my back!\n");
+}
