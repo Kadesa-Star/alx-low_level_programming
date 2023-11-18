125b6c592168d7e3a303891f4c3c172008f1928b
Kadesa-Star <133206743+Kadesa-Star@users.noreply.github.com>
2023-11-18 14:13:46 -0800

    free a linked list

---
+++ 0x12-singly_linked_lists/4-free_list.c
@@ -0,0 +1,21 @@
+#include "lists.h"
+/**
+ * free_list - frees a linked list
+ * @head: link to head
+ *
+ * Return: freed list
+ */
+void free_list(list_t *head)
+{
+	list_t *curr;
+	list_t *temp;
+
+	curr = head;
+
+	while (curr != NULL)
+	{
+		temp = curr;
+		curr = curr->next;
+		free(temp);
+	}
+}
