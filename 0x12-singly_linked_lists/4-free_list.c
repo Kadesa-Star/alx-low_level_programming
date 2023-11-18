2896c52a02975f4698730f68b5aceee6352eccd3
Kadesa-Star <133206743+Kadesa-Star@users.noreply.github.com>
2023-11-18 15:10:38 -0800

    frees linked list

--- 0x12-singly_linked_lists/4-free_list.c
+++ 0x12-singly_linked_lists/4-free_list.c
@@ -2,18 +2,17 @@
 /**
  * free_list - frees a linked list
  * @head: link to head
- *
- * Return: freed list
  */
 void free_list(list_t *head)
 {
-	list_t *temp;
 
 	if (head)
 	{
-		temp = head->next;
+		free_list(head->next);
+	}
+		if (head->str)
+		{
 		free(head->str);
 		free(head);
-		head = temp;
 	}
 }
