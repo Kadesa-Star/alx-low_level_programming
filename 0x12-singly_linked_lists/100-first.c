ae9324595acb17d3a157fe1c9da0dd87f7c240dd
Kadesa-Star <133206743+Kadesa-Star@users.noreply.github.com>
2023-11-18 16:09:51 -0800

    The Hare and the Tortoise

--- 0x12-singly_linked_lists/100-first.c
+++ 0x12-singly_linked_lists/100-first.c
@@ -11,5 +11,5 @@ void myfunc(void)__attribute__((constructor));
 void myfunc(void)
 {
 	printf("You're beat! and yet, you must allow,\n");
-	printf("bore my house upon my back!\n");
+	printf("I bore my house upon my back!\n");
 }
