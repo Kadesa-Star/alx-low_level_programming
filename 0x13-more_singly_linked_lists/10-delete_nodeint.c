26cfdb565bd6630ec3c81ce9a9bd0c8abebd6ee9
Kadesa-Star <133206743+Kadesa-Star@users.noreply.github.com>
2023-11-21 09:03:14 -0800

    deleting at a specified index

--- 0x13-more_singly_linked_lists/10-delete_nodeint.c
+++ 0x13-more_singly_linked_lists/10-delete_nodeint.c
@@ -19,7 +19,7 @@ int delete_nodeint_at_index(listint_t **head, unsigned int index)
 	temp = *head;
 	prev = NULL;
 
-	for j = 0; j < index && temp != NULL; j++)
+	for (j = 0; j < index && temp != NULL; j++)
 	{
 		prev = temp;
 		temp = temp->next;
