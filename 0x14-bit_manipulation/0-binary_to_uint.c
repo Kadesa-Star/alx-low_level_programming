a9aeb516dac05d569d769982ece472883e4eaf5b
Kadesa-Star <133206743+Kadesa-Star@users.noreply.github.com>
2023-11-23 11:32:36 -0800

    address where loop starts

--- 0x14-bit_manipulation/0-binary_to_uint.c
+++ 0x14-bit_manipulation/0-binary_to_uint.c
@@ -7,19 +7,23 @@
 unsigned int binary_to_uint(const char *b)
 {
 	unsigned int res = 0;
+	int c;
 
 	while (b == NULL)
 	{
 		return (0);
 	}
-	if (*b != '\0')
+	for (c = 0; b[c] != '\0'; c++)
 	{
-		if (*b != '0' && *b != '1')
+		if (b[c] != '0' && b[c] != '1')
 		{
 			return (0);
 		}
-		res = (res << 1) | (*b - '0');
-		b++;
+		res = (res << 1);
+		if (b[c] == '1')
+		{
+			res = res + 1;
+		}
 	}
 	return (res);
 }
