56f6f0bf542aea133bf46295ef407139d5e149b3
Kadesa-Star <133206743+Kadesa-Star@users.noreply.github.com>
2024-01-25 12:21:25 -0800

    freeing the next node

---
+++ 0x17-doubly_linked_lists/4-free_dlistint.c
@@ -0,0 +1,17 @@
+#include "lists.h"
+/**
+ * free_dlistint - frees a ddl
+ * @head: pointer to head
+ */
+void free_dlistint(dlistint_t *head)
+{
+	dlistint_t *curr = head;
+	dlistint_t *nextnode;
+
+	while (curr != NULL)
+	{
+		nextnode = curr->next;
+		free(curr);
+		curr = nextnode
+	}
+}
