d8eadd1c539b26c01fb12ba9829f36a32098f74c
Kadesa-Star <133206743+Kadesa-Star@users.noreply.github.com>
2024-01-26 00:51:10 -0800

    delete at an index

--- 0x17-doubly_linked_lists/8-delete_dnodeint.c
+++ 0x17-doubly_linked_lists/8-delete_dnodeint.c
@@ -11,7 +11,7 @@ int delete_dnodeint_at_index(dlistint_t **head, unsigned int index)
 	dlistint_t *curr, *temp;
 	unsigned int count = 0;
 
-	if (head == NULL)
+	if (head == NULL || *head == NULL)
 		return (-1);
 	curr = *head;
 	if (index == 0)
