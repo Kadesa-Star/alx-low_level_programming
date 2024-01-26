5a965997ccb069ec5e56a5c09384bae88e6aded8
Kadesa-Star <133206743+Kadesa-Star@users.noreply.github.com>
2024-01-26 00:11:56 -0800

    inseerting at an index

--- 0x17-doubly_linked_lists/7-insert_dnodeint.c
+++ 0x17-doubly_linked_lists/7-insert_dnodeint.c
@@ -12,7 +12,7 @@ dlistint_t *insert_dnodeint_at_index(dlistint_t **h, unsigned int idx, int n)
 	dlistint_t *new, *curr;
 	unsigned int count = 0;
 
-	if (*h == NULL)
+	if (h == NULL)
 		return (NULL);
 
 	curr = *h;
