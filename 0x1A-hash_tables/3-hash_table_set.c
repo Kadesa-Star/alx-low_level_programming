375f6674b2e3e3d2601e4bce4b5968d2c929c326
Kadesa-Star <133206743+Kadesa-Star@users.noreply.github.com>
2024-02-21 11:55:50 -0800

    add an element to a hash table

--- 0x1A-hash_tables/3-hash_table_set.c
+++ 0x1A-hash_tables/3-hash_table_set.c
@@ -13,7 +13,7 @@ int hash_table_set(hash_table_t *ht, const char *key, const char *value)
 	hash_node_t *new_node = NULL, *current_node = NULL;
 	unsigned long int index;
 
-	if (ht == NULL || key == NULL || *key == '\0')
+	if (ht == NULL || key == NULL || *key == '\0' || value == NULL)
 		return (0);
 	index = key_index((const unsigned char *)key, ht->size);
 	current_node = ht->array[index];
@@ -35,10 +35,18 @@ int hash_table_set(hash_table_t *ht, const char *key, const char *value)
 	new_node->key = strdup(key);
 	if (new_node->value == NULL)
 	{
+		free(new_node);
+		return (0);
+	}
+
+	new_node->value = strdup(value);
+	if (new_node->value == NULL)
+	{
 		free(new_node->key);
 		free(new_node);
 		return (0);
 	}
+
 	new_node->next = ht->array[index];
 	ht->array[index] = new_node;
 	return (1);
