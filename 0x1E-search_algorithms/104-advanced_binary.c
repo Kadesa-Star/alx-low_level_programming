2d7c2027657a3ddce733c19a4f7e3486aa54f89d
Kadesa-Star <133206743+Kadesa-Star@users.noreply.github.com>
2024-07-17 22:37:32 +0300

    advanced binary search

---
+++ 0x1E-search_algorithms/104-advanced_binary.c
@@ -0,0 +1,68 @@
+#include "search_algos.h"
+
+/**
+ * advanced_binary - Searches for a value in a sorted array of integers
+ *                   using advanced binary search.
+ * @array: A pointer to the first element of the array to search.
+ * @size: The number of elements in the array.
+ * @value: The value to search for.
+ *
+ * Return: If the value is not present or the array is NULL, -1.
+ *         Otherwise, the index where the value is located.
+ *
+ * Description: Prints the subarray being searched after each change.
+ */
+int advanced_binary(int *array, size_t size, int value)
+{
+	if (array == NULL || size == 0)
+		return (-1);
+
+	return (advanced_binary_recursive(array, 0, size - 1, value));
+}
+
+/**
+ * advanced_binary_recursive - Recursive helper function for advanced_binary.
+ * @array: A pointer to the first element of the array to search.
+ * @left: The starting index of the [sub]array to search.
+ * @right: The ending index of the [sub]array to search.
+ * @value: The value to search for.
+ *
+ * Return: If the value is not present or the array is NULL, -1.
+ *         Otherwise, the index where the value is located.
+ *
+ * Description: Prints the subarray being searched after each change.
+ */
+int advanced_binary_recursive(int *array, size_t left, size_t right, int value)
+{
+	size_t mid, i;
+
+	if (left > right)
+		return (-1);
+
+	mid = left + (right - left) / 2;
+
+	printf("Searching in array: ");
+	for (i = left; i <= right; i++)
+	{
+		if (i < right)
+			printf("%d, ", array[i]);
+		else
+			printf("%d\n", array[i]);
+	}
+
+	if (array[mid] == value)
+	{
+		if (mid == 0 || array[mid - 1] != value)
+			return (mid);
+		else
+			return (advanced_binary_recursive(array, left, mid, value));
+	}
+	else if (array[mid] > value)
+	{
+		return (advanced_binary_recursive(array, left, mid - 1, value));
+	}
+	else
+	{
+		return (advanced_binary_recursive(array, mid + 1, right, value));
+	}
+}
