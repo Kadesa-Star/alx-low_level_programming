3c425eabdb8f08d4d97089d8fb3ef30876c073dc
Kadesa-Star <133206743+Kadesa-Star@users.noreply.github.com>
2024-07-17 05:05:22 +0300

    header file

---
+++ 0x1E-search_algorithms/search_algos.h
@@ -0,0 +1,10 @@
+#ifndef SEARCH_ALGOS.H
+#define SEARCH_ALGOS.H
+
+
+#include <stdio.h>
+
+int linear_search(int *array, size_t size, int value);
+int binary_search(int *array, size_t size, int value);
+
+#endif /* SEARCH_ALGOS_H */
